5ad78632f0c51d672748a2db9027ad085b4f164d
RafaelOviedo <oviedo.rp@gmail.com>
2022-02-20 13:02:17 -0300

    update(Navbar): removing styles

--- src/components/NavBar/navBar.js
+++ src/components/NavBar/navBar.js
@@ -4,22 +4,13 @@ import styles from "./navBar.module.css";
 import 'boxicons';
 
 export default function NavBar() {
-    const [toggleButton, setToggleButton] = useState(true);
-
-    function handleToggle() {
-        setToggleButton(false);
-    };
-
     return (
         <nav className={styles.navBarContainer}>
             <Link to="/" className={styles.arrowButton}>
                 &#10094; Back
             </Link>
             <div className={styles.navbarLogo}>MOVIE APP</div>
-            <button className={toggleButton ? styles.menuIcon : styles.notVisible} onClick={() => handleToggle()}>
-                <box-icon name='menu'></box-icon>
-            </button>
-            <ul className={toggleButton ? styles.notVisible : styles.navbarList}>
+            <ul className={styles.navbarList}>
                 <Link exact to="/home" className={styles.listItem}>
                     Home
                 </Link>
